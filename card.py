188b02bf1a927b9bf4857bfa01636432119457c7
stranger <153902036+itpofy2024o@users.noreply.github.com>
2025-03-17 02:01:28 +0800

    Update card.py

--- card.py
+++ card.py
@@ -3,6 +3,9 @@ import sys
 
 num = sys.argv[1]
 
+if int(num) > 39:
+    raise ValueError("shut down")
+
 variation = "A,2,3,4,5,6,7,8,9,T,J,Q,K".split(",")
 
 style = "C,D,H,S".split(",")
@@ -18,7 +21,7 @@ while len(ans)!=int(num):
         group_a = variation[2:9]
     else:
         group_a = variation[9:]
-    prob_b = round(np.random.randint(4)/4,2)
+    prob_b = round(np.random.randint(4000)/4000,2)
     if prob_b <=0.20:
         group_b = style[0]
     elif prob_b > 0.20 and prob_b <=0.40:
@@ -27,34 +30,11 @@ while len(ans)!=int(num):
         group_b = style[2]
     else:
         group_b = style[3]
-    if len("".join(ans))>=44:
-        last = "".join(ans)[-44:]
-        arr = []
-        sin = []
-        plu = []
-        for i in last:
-            if last.index(i) % 2 == 0:
-                sin.append(i)
-            else:
-                plu.append(i)
-        for i in range(len(sin)):
-            for k in range(len(plu)):
-                if i == k:
-                    h = sin[i]+plu[k]
-                    arr.append(h)
-        while True:
-            a_index = np.random.randint(len(group_a))    
-            b_index = np.random.randint(len(group_b))
-            out = group_a[a_index]+group_b[b_index]
-            if out not in arr:
-                if np.random.randint(100)/100 > 0.369:
-                    ans.append(out)
-                    break
-    else:
-        a_index = np.random.randint(len(group_a))
-        b_index = np.random.randint(len(group_b))
-        out = group_a[a_index]+group_b[b_index]
-        if np.random.randint(100)/100 > 0.369:
+    a_index = np.random.randint(len(group_a))
+    b_index = np.random.randint(len(group_b))
+    out = group_a[a_index]+group_b[b_index]
+    if np.random.randint(100)/100 > 0.369:
+        if out not in ans:
             ans.append(out)
 
 ans = "".join(ans)
